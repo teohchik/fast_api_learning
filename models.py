353c2d27ecdcd3ebca591e18c869df2dda460fef
teosha <dima.trotsiuk322@gmail.com>
2025-12-31 19:47:38 +0000

    created 3 basic pydantic models

---
+++ models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    telegram_id: int
+    username: str | None
+    first_name: str | None
+
+class Category(BaseModel):
+    id: int
+    title: str
+    user_id: User
+
+class Expense(BaseModel):
+    id: int
+    user_id: User
+    category_id: Category
+    amount: float
+    description: str
