a9348ff1e0853f474f0761f58bee0dea3202b736
teosha <dima.trotsiuk322@gmail.com>
2026-01-03 23:15:39 +0000

    Added new endpoint for change user data

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.crud.users import add_user
+from db.crud.users import add_user_db, update_user_db
 from db.deps import get_db
-from schemas.user import UserResponse, UserCreate
+from schemas.user import UserResponse, UserCreate, UserUpdate
 
 users_router = APIRouter(
     prefix="/users",
@@ -13,9 +13,8 @@ users_router = APIRouter(
 
 @users_router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def create_user(new_user_data: UserCreate, db: AsyncSession = Depends(get_db)):
-    return await add_user(new_user_data, db)
+    return await add_user_db(new_user_data, db)
 
-@users_router.get("/{telegram_id}")
-def get_full_user_information_by_telegram_id(telegram_id: int):
-    # Need to get user from the database here
-    return "Full user information"
+@users_router.patch("/{telegram_id}", response_model=UserResponse)
+async def update_user(telegram_id: int, update_data: UserUpdate, db: AsyncSession = Depends(get_db)):
+    return await update_user_db(telegram_id, update_data, db)
