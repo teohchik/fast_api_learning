7930b9393ff0c073caa1b2638ac07fc42693162e
teosha <dima.trotsiuk322@gmail.com>
2026-01-03 23:15:44 +0000

    Added new endpoint for change user data

--- db/crud/users.py
+++ db/crud/users.py
@@ -1,13 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models.users import User
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException, status
-from schemas.user import UserCreate
+from schemas.user import UserCreate, UserUpdate
 from schemas.user import UserResponse
 
 
-async def add_user(new_user_data: UserCreate, db: AsyncSession) -> UserResponse:
+async def add_user_db(new_user_data: UserCreate, db: AsyncSession) -> UserResponse:
     try:
         db_user = User(**new_user_data.model_dump())
         db.add(db_user)
@@ -18,7 +19,23 @@ async def add_user(new_user_data: UserCreate, db: AsyncSession) -> UserResponse:
         print("IntegrityError occurred while adding a new user:", exc)
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Користувач з таким Telegram ID вже існує"
+            detail="User with this telegram_id already exists."
         )
 
     return UserResponse.model_validate(db_user)
+
+async def update_user_db(telegram_id: int, update_data: UserUpdate, db: AsyncSession):
+    query = select(User).where(User.telegram_id == telegram_id)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    update_dict = update_data.model_dump(exclude_unset=True)  # тільки те, що передали
+    for key, value in update_dict.items():
+        setattr(user, key, value)
+
+    await db.commit()
+    await db.refresh(user)
+    return UserResponse.model_validate(user)
