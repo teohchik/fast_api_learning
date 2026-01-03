f99b2425d296c0c2862f80b31db332ccb17f1676
teosha <dima.trotsiuk322@gmail.com>
2026-01-03 23:16:50 +0000

    Updated SQLAlchemy User model

--- db/models/users.py
+++ db/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.base import Base
@@ -7,9 +7,9 @@ from db.base import Base
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
-    username: Mapped[str | None] = mapped_column(String(length=100), nullable=True)
-    first_name: Mapped[str | None] = mapped_column(String(length=100), nullable=True)
-    last_name: Mapped[str | None] = mapped_column(String(length=100), nullable=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False, index=True)
+    username: Mapped[str | None] = mapped_column(String(length=255), nullable=True)
+    first_name: Mapped[str] = mapped_column(String(length=255), nullable=False)
+    last_name: Mapped[str | None] = mapped_column(String(length=255), nullable=True)
     categories = relationship("Category", back_populates="user")
     expenses = relationship("Expense", back_populates="user")
