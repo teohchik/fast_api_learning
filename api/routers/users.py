d5ced5f9c61187cccf10c4b8151a2be5d220d914
teosha <dima.trotsiuk322@gmail.com>
2026-01-05 17:58:49 +0000

    fixed output

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Body
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.crud.users import add_user_db, update_user_db
@@ -11,9 +11,19 @@ users_router = APIRouter(
 )
 
 
+@users_router.get("/", response_model=UserResponse)
+async def get_users(db: AsyncSession = Depends(get_db)):
+    # Placeholder for getting users logic
+    pass
+
+
 @users_router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def create_user(new_user_data: UserCreate, db: AsyncSession = Depends(get_db)):
+async def create_user(new_user_data: UserCreate = Body(openapi_examples={
+    "1": {"summary": "Example User", "value":
+        {"telegram_id": 123456789, "username": "example_user", "first_name": "example", "last_name": "example"}}}),
+        db: AsyncSession = Depends(get_db)):
     return await add_user_db(new_user_data, db)
+
 
 @users_router.patch("/{telegram_id}", response_model=UserResponse)
 async def update_user(telegram_id: int, update_data: UserUpdate, db: AsyncSession = Depends(get_db)):
