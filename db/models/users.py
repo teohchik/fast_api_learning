0e4e04f912be278a041fb7e90842f937b94ee3a4
teosha <dima.trotsiuk322@gmail.com>
2026-01-05 13:27:45 +0000

    deleted useless code

--- db/models/users.py
+++ db/models/users.py
@@ -7,9 +7,9 @@ from db.base import Base
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False, index=True)
-    username: Mapped[str | None] = mapped_column(String(length=255), nullable=True)
-    first_name: Mapped[str] = mapped_column(String(length=255), nullable=False)
-    last_name: Mapped[str | None] = mapped_column(String(length=255), nullable=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
+    username: Mapped[str | None] = mapped_column(String(length=255))
+    first_name: Mapped[str] = mapped_column(String(length=255))
+    last_name: Mapped[str | None] = mapped_column(String(length=255))
     categories = relationship("Category", back_populates="user")
     expenses = relationship("Expense", back_populates="user")
