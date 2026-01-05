2bd047657eb5e2f6ec1c0434cfec73d38e5406ab
teosha <dima.trotsiuk322@gmail.com>
2026-01-05 18:00:37 +0000

    added new router "category_router"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from api.routers.categories import category_router
 from api.routers.expenses import expenses_router
 from api.routers.users import users_router
 from db.init_db import init_db
@@ -16,6 +17,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(users_router)
 app.include_router(expenses_router)
+app.include_router(category_router)
 
 
 if __name__ == "__main__":
