acaf0532d7d8e37a28befa507c63096e64d098f9
teosha <dima.trotsiuk322@gmail.com>
2026-01-06 18:13:03 +0000

    Created depend on pagination

---
+++ api/dependencies.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from fastapi import Depends, Query
+from pydantic import BaseModel
+
+class PaginationParams(BaseModel):
+    page: Annotated[int | None, Query(1, ge=1)]
+    per_page: Annotated[int | None, Query(None, ge=1, le=100)]
+
+PaginationDep = Annotated[PaginationParams, Depends()]
