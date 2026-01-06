8b3a5efe5973d929e72215eb60c901833339454c
teosha <dima.trotsiuk322@gmail.com>
2026-01-06 21:48:35 +0000

    added new method "get_by_user_id" which works with "get_gy_filter"

--- db/repositories/categories.py
+++ db/repositories/categories.py
@@ -1,6 +1,14 @@
+from api.dependencies import PaginationParams
 from db.models.categories import Category
 from db.repositories.base import BaseRepository
 
 
 class CategoriesRepository(BaseRepository):
     model = Category
+
+    async def get_by_user_id(self, user_id: int, pagination: PaginationParams | None = None):
+        return await self.get_by_filters(
+            filters={"user_id": user_id},
+            pagination=pagination,
+            order_by=Category.created_at,
+        )
