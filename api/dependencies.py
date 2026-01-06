273022ecb96329eef04b2826c1ecb5447214a825
teosha <dima.trotsiuk322@gmail.com>
2026-01-06 21:50:28 +0000

    The default value for "per_page" was 20

--- api/dependencies.py
+++ api/dependencies.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(1, ge=1)]
-    per_page: Annotated[int | None, Query(None, ge=1, le=100)]
+    per_page: Annotated[int | None, Query(20, ge=1, le=100)]
 
 PaginationDep = Annotated[PaginationParams, Depends()]
