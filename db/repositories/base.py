e70f46f9628734ac38e29f9eb8e52902a15c3fbf
teosha <dima.trotsiuk322@gmail.com>
2026-01-06 21:11:58 +0000

    Added function edit_by_id

--- db/repositories/base.py
+++ db/repositories/base.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
 
 class BaseRepository:
     model = None
@@ -7,7 +9,7 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
-    async def get_all(self, pagination = None):
+    async def get_all(self, pagination=None):
         query = select(self.model)
         if pagination is not None:
             per_page = pagination.per_page or 10
@@ -23,5 +25,18 @@ class BaseRepository:
     async def add(self, data: BaseModel):
         db_obj = self.model(**data.model_dump())
         self.session.add(db_obj)
+        await self.session.flush()
+        return db_obj
+
+    async def edit_by_id(self, data: BaseModel, obj_id: int):
+        query = select(self.model).where(self.model.id == obj_id)
+        result = await self.session.execute(query)
+        db_obj = result.scalar_one_or_none()
+        if not db_obj:
+            raise NoResultFound(f"{self.model.__name__} not found")
+
+        for key, value in data.model_dump(exclude_unset=True).items():
+            setattr(db_obj, key, value)
+
         await self.session.flush()
         return db_obj
