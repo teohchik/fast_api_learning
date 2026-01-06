9de80c43752553969bd57856a9f2ef1735fc9fc9
teosha <dima.trotsiuk322@gmail.com>
2026-01-06 18:10:43 +0000

    Formated file

--- api/routers/categories.py
+++ api/routers/categories.py
@@ -1,6 +1,9 @@
+from typing import Annotated
+
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.dependencies import PaginationParams, PaginationDep
 from db.crud.categories import create_category_db, get_category_db
 from db.deps import get_db
 from schemas.category import CategoryResponse, CategoryCreate
@@ -10,10 +13,12 @@ category_router = APIRouter(
     tags=["Categories"]
 )
 
+
+@category_router.get("/", response_model=list[CategoryResponse], status_code=status.HTTP_200_OK)
+async def get_all_categories(pagination: PaginationDep, user_id: int, db: AsyncSession = Depends(get_db)):
+    return await get_category_db(pagination, user_id, db)
+
+
 @category_router.post("/", response_model=CategoryResponse, status_code=status.HTTP_201_CREATED)
 async def create_category(new_category_data: CategoryCreate, db: AsyncSession = Depends(get_db)):
     return await create_category_db(new_category_data, db)
-
-@category_router.get("/", response_model=list[CategoryResponse], status_code=status.HTTP_200_OK)
-async def get_all_categories(user_id: int, db: AsyncSession = Depends(get_db)):
-    return await get_category_db(user_id, db)
