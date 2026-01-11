340b3e554dc552fb670aad31672918d53f798119
teosha <dima.trotsiuk322@gmail.com>
2026-01-11 19:58:02 +0000

    function delete_by_id has been added to CategoriesRepository

--- src/db/repositories/categories.py
+++ src/db/repositories/categories.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
 from src.api.deps import PaginationParams
 from src.db.models.categories import Category
 from src.db.repositories.base import BaseRepository
@@ -13,4 +16,19 @@ class CategoriesRepository(BaseRepository):
             pagination=pagination,
             order_by=Category.created_at,
             user_id=user_id,
+            visible=True
         )
+
+    async def delete_by_id(self, category_id: int):
+        query = select(self.model).where((self.model.id == category_id) & (self.model.visible == True))
+
+        result = await self.session.execute(query)
+        db_obj = result.scalar_one_or_none()
+
+        if not db_obj:
+            raise NoResultFound(f"Category {category_id} not found")
+
+        # Soft delete
+        db_obj.visible = False
+        await self.session.flush()  # commit робиться зовні
+        return self.schema.model_validate(db_obj)
