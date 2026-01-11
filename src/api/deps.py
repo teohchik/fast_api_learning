26c769f80a942bd39e619d56e6ffa184409898cd
teosha <dima.trotsiuk322@gmail.com>
2026-01-11 19:09:00 +0000

    renamed

--- src/api/deps.py
+++ src/api/deps.py
@@ -1,9 +1,12 @@
-from typing import Annotated
+from typing import Annotated, TypeAlias
+
 from fastapi import Depends, Query
 from pydantic import BaseModel
+
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(1, ge=1)]
     per_page: Annotated[int | None, Query(20, ge=1, le=100)]
 
-PaginationDep = Annotated[PaginationParams, Depends()]
+PaginationDep: TypeAlias = Annotated[PaginationParams, Depends()]
+
