a1a876b266b9fbe965def183599f95a6ee1cce5b
teosha <dima.trotsiuk322@gmail.com>
2026-01-24 22:02:32 +0000

    fix:

--- src/connectors/redis_connector.py
+++ src/connectors/redis_connector.py
@@ -20,9 +20,10 @@ class RedisManager:
         await self.redis.delete(key)
 
     async def scan_delete(self, pattern: str):
-        async for key in self.redis.scan_iter(match=pattern):
-            print(f"Deleting key: {key.decode('utf-8')}")
-            await self.redis.delete(key)
+        if self.redis:
+            async for key in self.redis.scan_iter(match=pattern):
+                print(f"Deleting key: {key.decode('utf-8')}")
+                await self.redis.delete(key)
 
     async def disconnect(self):
         if self.redis:
