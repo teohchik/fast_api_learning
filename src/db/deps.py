45396f8180c8a720668694ce520385ab58ff5b87
teosha <dima.trotsiuk322@gmail.com>
2026-01-24 17:00:35 +0000

    refactoring

--- src/db/deps.py
+++ src/db/deps.py
@@ -6,11 +6,9 @@ from src.db.db_manager import DBManager
 from src.db.session import AsyncSessionLocal
 
 
-def get_db_manager():
-    return DBManager(session_factory=AsyncSessionLocal)
-
 async def get_db():
-    async with get_db_manager() as db:
+    async with DBManager(session_factory=AsyncSessionLocal) as db:
         yield db
+
 
 DBDep: TypeAlias = Annotated[DBManager, Depends(get_db)]
